6ed873d6d6992f41e5342429af732759217e6891
Carlj0666 <carlj1881@yahoo.com>
2022-04-06 12:08:04 -0700

    Added link to newest appPowerpill - a game

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -9,7 +9,7 @@ class Portfolio extends React.Component{
                     <h2>Here are some Youtube links to a few of my portfolio projects. You can also access the code on
                         my Github.</h2>
                     <br></br>
-                    <h5>Make sure your volume is down before hitting the link!</h5>
+                    <a href="https://powerpill.herokuapp.com" className="btn-extlink" target="_blank" rel="noreferrer">Powerpill</a>
                     <a href="https://youtu.be/nX1EyzE3tzY" className="btn-extlink" target="_blank" rel="noreferrer">Know Soft Char Builder (Youtube)</a>
                     <a href="https://github.com/Carlj0666" className="btn-extlink" target="_blank" rel="noreferrer">Github</a>
                 </div>
